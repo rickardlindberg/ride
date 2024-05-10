61affd920319bbacd8642f0fbdc0f1290c08ad61
Rickard Lindberg <rickard@rickardlindberg.me>
2024-05-10 21:48:41 +0200

    Embryo to TreeView.

--- ride.py
+++ ride.py
@@ -4,6 +4,45 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GLib
 
+class TreeView:
+
+    """
+    >>> tree_view = TreeView()
+    >>> Directory.create_null(paths={
+    ...     ".": ["folder1", "file2"],
+    ...     "./folder1": ["file11"],
+    ... }).populate_tree_view(tree_view)
+    >>> tree_view.render()
+    .
+    -- folder1
+    ---- file11
+    -- file2
+    """
+
+    def __init__(self):
+        self.items = []
+        self.indent = 0
+
+    def begin_tree(self, directory):
+        self.add(directory.name())
+        self.indent += 1
+
+    def leaf(self, file):
+        self.add(file.name())
+
+    def end_tree(self, directory):
+        self.indent -= 1
+
+    def add(self, name):
+        self.items.append((self.indent, name))
+
+    def render(self):
+        for indent, name in self.items:
+            if indent > 0:
+                print(f"{'--'*indent} {name}")
+            else:
+                print(name)
+
 class Directory:
 
     """
@@ -24,19 +63,22 @@ class Directory:
         return cls(os=os, path=path)
 
     @classmethod
-    def create_null(cls):
+    def create_null(cls, paths={".": ["folder1", "file1"]}):
         class NullPath:
             def isdir(self, path):
                 return "folder" in path
         class NullOs:
             path = NullPath()
             def listdir(self, path):
-                return ["folder1", "file1"]
+                return paths.get(path, [])
         return cls(os=NullOs(), path=".")
 
     def __init__(self, os, path):
         self.os = os
         self.path = path
+
+    def name(self):
+        return os.path.basename(self.path)
 
     def children(self):
         def factory(path):
@@ -49,6 +91,12 @@ class Directory:
             for x in self.os.listdir(self.path)
         ]
 
+    def populate_tree_view(self, tree_view):
+        tree_view.begin_tree(self)
+        for child in self.children():
+            child.populate_tree_view(tree_view)
+        tree_view.end_tree(self)
+
     def __repr__(self):
         return f"Directory(path={self.path!r})"
 
@@ -56,6 +104,12 @@ class File:
 
     def __init__(self, path):
         self.path = path
+
+    def name(self):
+        return os.path.basename(self.path)
+
+    def populate_tree_view(self, tree_view):
+        tree_view.leaf(self)
 
     def __repr__(self):
         return f"File(path={self.path!r})"
