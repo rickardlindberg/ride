f066ac0d7120d4de9a5d4e295dfa3ec6062580ee
Rickard Lindberg <rickard@rickardlindberg.me>
2024-05-10 15:23:55 +0200

    Add motion event handler.

--- ride.py
+++ ride.py
@@ -6,12 +6,20 @@ class Canvas(Gtk.DrawingArea):
 
     def __init__(self):
         Gtk.DrawingArea.__init__(self)
+        self.add_events(
+            self.get_events() |
+            Gdk.EventMask.POINTER_MOTION_MASK
+        )
         self.connect("draw", self.on_draw)
+        self.connect("motion-notify-event", self.on_motion_notify_event)
 
     def on_draw(self, widget, context):
         context.set_source_rgb(1, 0, 0)
         context.rectangle(10, 10, 10, 10)
         context.fill()
+
+    def on_motion_notify_event(self, widget, event):
+        print(event)
 
 if __name__ == "__main__":
     window = Gtk.Window()
