df9ef134b945f70ae6aaa8cb28369f2e598368d1
Rickard Lindberg <rickard@rickardlindberg.me>
2024-05-10 21:25:51 +0200

    Embryo to directory and file objects.

--- ride.py
+++ ride.py
@@ -1,6 +1,64 @@
+import os
+
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GLib
+
+class Directory:
+
+    """
+    >>> d = Directory.create_null()
+
+    >>> d
+    Directory(path='.')
+
+    >>> d.children()
+    [Directory(path='./folder1'), File(path='./file1')]
+
+    >>> isinstance(Directory.create().children(), list)
+    True
+    """
+
+    @classmethod
+    def create(cls, path="."):
+        return cls(os=os, path=path)
+
+    @classmethod
+    def create_null(cls):
+        class NullPath:
+            def isdir(self, path):
+                return "folder" in path
+        class NullOs:
+            path = NullPath()
+            def listdir(self, path):
+                return ["folder1", "file1"]
+        return cls(os=NullOs(), path=".")
+
+    def __init__(self, os, path):
+        self.os = os
+        self.path = path
+
+    def children(self):
+        def factory(path):
+            if self.os.path.isdir(path):
+                return Directory(os=self.os, path=path)
+            else:
+                return File(path)
+        return [
+            factory(os.path.join(self.path, x))
+            for x in self.os.listdir(self.path)
+        ]
+
+    def __repr__(self):
+        return f"Directory(path={self.path!r})"
+
+class File:
+
+    def __init__(self, path):
+        self.path = path
+
+    def __repr__(self):
+        return f"File(path={self.path!r})"
 
 class Canvas(Gtk.DrawingArea):
 
